0c197a541281e7ca52f4b76f8f1959ca2cf66fab
J1nx <admin@j1nx.nl>
2013-01-19 11:10:15 +0000

    [f16ref] Fix bluem3 config

--- env.sh
+++ env.sh
@@ -2,11 +2,11 @@ export ARCH=arm
 export CROSS_COMPILE=/root/buildroot-AmLogic/output/external-toolchain/bin/arm-none-linux-gnueabi-
 #export CROSS_COMPILE=arm-none-linux-gnueabi-
 make clean
-make meson_reff16_defconfig
+make meson_reff16_bluem3_defconfig
 make menuconfig
 #make vmlinux
 
 TOP=${PWD}
 export MKIMAGE=${TOP}/arch/arm/boot/mkimage
 make uImage
-make modules
+#make modules
