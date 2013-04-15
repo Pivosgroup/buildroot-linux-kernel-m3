798bc939a6d15168cd60dbb5ad1e0253faccb298
Re van Dorst <opensource@vdorst.com>
2013-04-15 13:16:14 +0200

    XIOS: Set Ledstatus trigger to rc when enabled in kernel config.

--- arch/arm/mach-meson3/board-stv-mbx-mc.c
+++ arch/arm/mach-meson3/board-stv-mbx-mc.c
@@ -115,7 +115,11 @@ static struct gpio_led aml_led_pins[] = {
 	},
 	{
 		.name		 = "Statusled",
+#if defined(CONFIG_LEDS_TRIGGER_REMOTE_CONTROL)
+		.default_trigger = "rc", 
+#else
 		.default_trigger = "none",
+#endif
 		.gpio		 = GPIO_LED_STATUS,
 		.active_low	 = 1,
 	},
