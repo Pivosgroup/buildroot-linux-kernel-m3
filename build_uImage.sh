359ce2fe7c4a28b627e895472b9f5c3094da6ae5
Trent Nelson <trent.nelson@pivosgroup.com>
2014-03-27 23:17:00 -0600

    Compile both of our wifi drivers as modules.
    m3v1: RTL8192CU
    m3v2: RTL8188EU

--- build_uImage.sh
+++ build_uImage.sh
@@ -5,8 +5,12 @@ make modules -j4
 mkdir -p ramdisk/root/boot
 cp -vf drivers/amlogic/ump/ump.ko ramdisk/root/boot/
 cp -vf drivers/amlogic/mali/mali.ko ramdisk/root/boot/
+cp -vf drivers/amlogic/wifi/rtl8xxx_EU/8188eu.ko ramdisk/root/boot/
+cp -vf drivers/amlogic/wifi/rtl8xxx_CU/8192cu.ko ramdisk/root/boot/
 mkdir -p ramdisk/root_release/boot
 cp -vf drivers/amlogic/ump/ump.ko ramdisk/root_release/boot/
 cp -vf drivers/amlogic/mali/mali.ko ramdisk/root_release/boot/
+cp -vf drivers/amlogic/wifi/rtl8xxx_EU/8188eu.ko ramdisk/root_release/boot/
+cp -vf drivers/amlogic/wifi/rtl8xxx_CU/8192cu.ko ramdisk/root_release/boot/
 make uImage -j4
 cp -rf arch/arm/boot/uImage ../out/target/product/xiosm3/
